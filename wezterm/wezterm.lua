e435a1348389e131b48d0ebdb9960096e5e23f28
Daniel <djpgonzaga@gmail.com>
2025-05-11 18:59:56 -0300

    feat: update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,47 +3,35 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
 config = {
-
+  -- performance
   front_end = "WebGpu",
-  max_fps = 120,
-  adjust_window_size_when_changing_font_size = false,
-  automatically_reload_config = true,
-
-  freetype_load_flags = 'NO_HINTING',
-  freetype_load_target = 'Light',
-  -- freetype_render_target = 'HorizontalLcd',
+  max_fps = 144,
+  animation_fps = 10,
 
   enable_tab_bar = false,
-  use_fancy_tab_bar = false,
+
+  -- theme
+  color_scheme = "Poimandres Alternate",
+  color_scheme_dirs = { '~/.config/wezterm/theme/poimandres.toml' },
+
+  -- appearance
   window_close_confirmation = "NeverPrompt",
   window_decorations = "RESIZE|MACOS_FORCE_ENABLE_SHADOW",
   window_background_opacity = 0.60,
+  window_padding = { left = 16, right = 16, top = 16, bottom = 16 },
   macos_window_background_blur = 30,
 
-  color_scheme = "Poimandres",
-
-  font = wezterm.font("MesloLGS Nerd Font Mono"),
+  -- font
   font_size = 16,
+  font = wezterm.font("MesloLGS Nerd Font Mono"),
+  freetype_load_flags = 'NO_HINTING',
+  freetype_load_target = 'Light',
 
-  window_padding = {
-    left = 16,
-    right = 16,
-    top = 16,
-    bottom = 16,
-  },
-
+  -- keybind
   leader = { key = " ", mods = "CTRL", timeout_milliseconds = 1000 },
   keys = {
-    {
-      key = 's',
-      mods = 'LEADER',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'v',
-      mods = 'LEADER',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
+    { key = 's', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'v', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     {
       key = "h",
       mods = "LEADER",
@@ -56,16 +44,8 @@ config = {
         end
       end),
     },
-    {
-      key = "j",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Down")
-    },
-    {
-      key = "k",
-      mods = "LEADER",
-      action = wezterm.action.ActivatePaneDirection("Up")
-    },
+    { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
     {
       key = "l",
       mods = "LEADER",
@@ -83,9 +63,4 @@ config = {
 
 return config
 
-
--- alternative
--- color_scheme = "Catppuccin Mocha",
--- font = wezterm.font 'JetBrains Mono',
--- default_cursor_style = "SteadyUnderline",
--- default_cursor_style = "SteadyBlock",
+-- default_cursor_style
